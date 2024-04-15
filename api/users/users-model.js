bdce2f1166583edeb3846ef0815a6fe0f6b9190e
mkaufman385 <mkaufman385@gmail.com>
2024-04-15 10:13:58 -0400

    wip

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -4,24 +4,17 @@ function find() {
   return db("users");
 }
 
-function findById(user_id) {
+function findBy(user_id) {
   return db("users").where("users.user_id", user_id).first();
 }
 
 async function add({ username, password }) {
-  let created_user_id;
-  await db.transaction(async (trx) => {
-    const [user_id] = await trx("users").insert({
-      username,
-      password,
-    });
-    created_user_id = user_id;
-  });
-  return findById(created_user_id);
+  const [user_id] = await db("users").insert({ username, password });
+  return findBy(user_id);
 }
 
 module.exports = {
   find,
-  findById,
+  findBy,
   add,
 };
