eccada0a75d3aab0bd449b4fa5c2bcf6837cb6a5
mkaufman385 <mkaufman385@gmail.com>
2024-04-15 09:36:25 -0400

    wip

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -8,30 +8,20 @@ function findById(user_id) {
   return db("users").where("users.user_id", user_id).first();
 }
 
-// async function add({ username, password }) {
-//   // done for you
-//   let created_user_id;
-//   await db.transaction(async (trx) => {
-//     let role_id_to_use;
-//     const [role] = await trx("roles").where("role_name", role_name);
-//     if (role) {
-//       role_id_to_use = role.role_id;
-//     } else {
-//       const [role_id] = await trx("roles").insert({ role_name: role_name });
-//       role_id_to_use = role_id;
-//     }
-//     const [user_id] = await trx("users").insert({
-//       username,
-//       password,
-//       role_id: role_id_to_use,
-//     });
-//     created_user_id = user_id;
-//   });
-//   return findById(created_user_id);
-// }
+async function add({ username, password }) {
+  let created_user_id;
+  await db.transaction(async (trx) => {
+    const [user_id] = await trx("users").insert({
+      username,
+      password,
+    });
+    created_user_id = user_id;
+  });
+  return findById(created_user_id);
+}
 
 module.exports = {
   find,
   findById,
-  // add,
+  add,
 };
