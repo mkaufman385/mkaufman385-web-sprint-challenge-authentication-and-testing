48c764ffcfee2b2d63c90bca66537a3d596425ec
mkaufman385 <mkaufman385@gmail.com>
2024-04-15 11:25:52 -0400

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,14 +1,14 @@
-const router = require("express").Router();
+// const router = require("express").Router();
 
-const User = require("./users-model");
-const { restricted } = require("../middleware/restricted");
+// const User = require("./users-model");
+// const { restricted } = require("../middleware/restricted");
 
-router.get("/", restricted, (req, res, next) => {
-  User.find()
-    .then((users) => {
-      res.json(users);
-    })
-    .catch(next);
-});
+// router.get("/", restricted, (req, res, next) => {
+//   User.find()
+//     .then((users) => {
+//       res.json(users);
+//     })
+//     .catch(next);
+// });
 
-module.exports = router;
+// module.exports = router;
