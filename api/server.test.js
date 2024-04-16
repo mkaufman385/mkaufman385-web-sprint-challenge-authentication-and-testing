1c24bef83c0c7ab395ea9a54b67451d2530c5e06
mkaufman385 <mkaufman385@gmail.com>
2024-04-16 15:36:17 -0400

    wip

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,17 @@
+const db = require("../data/dbConfig");
+const Joke = require("../api/users/users-model");
+
 // Write your tests here
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+
+// beforeEach(async () => {
+//   await db.seed.run();
+// });
+
 test("sanity", () => {
   expect(true).toBe(true);
 });
@@ -10,3 +23,18 @@ test("environment is testing", () => {
 // dont forget beforeAll and beforeEach.
 
 //Do tests on the model first and then endpoints
+
+describe("find users", () => {
+  test("finds all users in table", async () => {
+    const result = await Joke.find();
+    expect(result).toHaveLength(0);
+    expect(result).toMatchObject([]);
+  });
+});
+
+describe("findById", () => {
+  test("resolves user by id", async () => {
+    const result = await Joke.findById();
+    expect(result);
+  });
+});
