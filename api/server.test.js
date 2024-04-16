53e17ba8ff1ef9c9fc71b766f7a897a44570ea88
mkaufman385 <mkaufman385@gmail.com>
2024-04-16 11:34:38 -0400

    wip

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,8 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+test("sanity", () => {
+  expect(true).toBe(false);
+});
+
+// dont forget beforeAll and beforeEach.
+
+//Do tests on the model first and then endpoints
